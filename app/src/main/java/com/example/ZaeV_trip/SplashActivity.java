ddf162d0ecf912d1dca30575bd1aefe3e370d326
CHOSIYEON <tldusdk5@naver.com>
2022-07-22 23:45:12 +0900

    feat : splash activity 추가

---
+++ app/src/main/java/com/example/ZaeV_trip/SplashActivity.java
@@ -0,0 +1,43 @@
+package com.example.ZaeV_trip;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        ImageView gif_image = (ImageView) findViewById(R.id.gif_image);
+        Glide.with(this).load(R.raw.animation_bird).into(gif_image);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+//                if (MySharedPreferences.getUserEmail(SplashActivity.this).length() != 0) {
+//                    Toast.makeText(getApplicationContext(), "자동 로그인 되었습니다.", Toast.LENGTH_SHORT).show();
+//                    Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+//                    startActivity(intent);
+//                } else {
+//                    Intent intent = new Intent(SplashActivity.this, IntroActivity.class);
+//                    startActivity(intent);
+//                }
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+
+    }
+
+}
