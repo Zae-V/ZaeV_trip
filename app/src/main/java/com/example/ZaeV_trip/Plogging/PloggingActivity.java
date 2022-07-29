3de4ce27880dbbe17e6737487a78e22f5db3754e
CHOSIYEON <tldusdk5@naver.com>
2022-07-29 16:38:50 +0900

    add : 플로깅 액티비티 추가 및 연결

---
+++ app/src/main/java/com/example/ZaeV_trip/Plogging/PloggingActivity.java
@@ -0,0 +1,16 @@
+package com.example.ZaeV_trip.Plogging;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.ZaeV_trip.R;
+
+public class PloggingActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_plogging);
+    }
+}
