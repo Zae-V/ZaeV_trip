87e8f5b1a0d8b650fe81cb3a605bbbd4e0d6e7d7
SeonDahye <81613151+SeonDahye@users.noreply.github.com>
2022-08-05 00:27:49 +0900

    firestore 추가

--- app/src/main/java/com/example/ZaeV_trip/Restaurant/RestaurantActivity.java
+++ app/src/main/java/com/example/ZaeV_trip/Restaurant/RestaurantActivity.java
@@ -6,12 +6,15 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.ImageView;
+import androidx.appcompat.widget.SearchView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.ZaeV_trip.R;
 import com.example.ZaeV_trip.Reusable.DetailReusableFragment;
@@ -35,24 +38,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RestaurantActivity extends AppCompatActivity {
-
     ArrayList<Restaurant> restaurants = new ArrayList<>();
     String local;
-
-    GridView gridView;
+    SearchView searchView;
+    RecyclerView list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant);
 
+        list = (RecyclerView) findViewById(R.id.restaurantList);
+        searchView = findViewById(R.id.restaurantSearchBar);
         Bundle extras = getIntent().getExtras();
 
         if(extras!=null){
             local = extras.getString("local");
         }
-
-        gridView = findViewById(R.id.touristSpotList);
 
         new Thread(new Runnable() {
             @Override
@@ -74,10 +76,23 @@ public class RestaurantActivity extends AppCompatActivity {
                                 }
                             }
                             RestaurantAdapter adapter = new RestaurantAdapter(RestaurantActivity.this, filteredRestaurant);
-                            gridView.setAdapter(adapter);
-                            gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                            list.setLayoutManager(new LinearLayoutManager(RestaurantActivity.this, RecyclerView.VERTICAL, false));
+                            list.setAdapter(adapter);
+                            searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
                                 @Override
-                                public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                                public boolean onQueryTextSubmit(String s) {
+                                    adapter.getFilter().filter(s);
+                                    return false;
+                                }
+
+                                @Override
+                                public boolean onQueryTextChange(String s) {
+                                    return false;
+                                }
+                            });
+                            adapter.setOnItemClickListener(new RestaurantAdapter.OnItemClickListener() {
+                                @Override
+                                public void onItemClick(View v, int i) {
                                     Bundle bundle = new Bundle();
                                     bundle.putString("name", filteredRestaurant.get(i).getTitle());
                                     bundle.putString("id", filteredRestaurant.get(i).getContentID());
--- app/src/main/java/com/example/ZaeV_trip/Restaurant/RestaurantAdapter.java
+++ app/src/main/java/com/example/ZaeV_trip/Restaurant/RestaurantAdapter.java
@@ -1,83 +1,209 @@
 package com.example.ZaeV_trip.Restaurant;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.example.ZaeV_trip.Cafe.CafeAdapter;
 import com.example.ZaeV_trip.R;
 import com.example.ZaeV_trip.TouristSpot.TouristSpotActivity;
+import com.example.ZaeV_trip.model.Cafe;
 import com.example.ZaeV_trip.model.Restaurant;
 import com.example.ZaeV_trip.model.TouristSpot;
+import com.example.ZaeV_trip.util.MySharedPreferences;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
-public class RestaurantAdapter extends BaseAdapter {
+public class RestaurantAdapter extends RecyclerView.Adapter<RestaurantAdapter.ViewHolder> implements Filterable {
+    RestaurantFilter filter = new RestaurantFilter();
     Context context;
     LayoutInflater inflater;
-
-    ArrayList<Restaurant> restaurants = new ArrayList<>();
+    ArrayList<Restaurant> restaurants;
+    ArrayList<Restaurant> filtered;
+    FirebaseFirestore mDatabase;
 
     public RestaurantAdapter() {
 
     }
 
-    public RestaurantAdapter(RestaurantActivity restaurantActivity) {
-        this.context = restaurantActivity;
+
+    public RestaurantAdapter(Context context, ArrayList<Restaurant> restaurants) {
+        this.context = context;
+        this.restaurants = new ArrayList<>(restaurants);
+        this.filtered = restaurants;
     }
 
-    public RestaurantAdapter(RestaurantActivity restaurantActivity, ArrayList<Restaurant> restaurants) {
-        this.context = restaurantActivity;
-        this.restaurants = restaurants;
-    }
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        context = parent.getContext();
+        inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
-    public void addItem(Restaurant item){
-        restaurants.add(item);
+        View view = inflater.inflate(R.layout.item_cafe, parent, false);
+        ViewHolder viewHolder = new ViewHolder(context, view);
+
+        return viewHolder;
     }
 
     @Override
-    public int getCount() {
-        return restaurants.size();
-    }
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
 
-    @Override
-    public Object getItem(int i) {
-        return restaurants.get(i);
-    }
-
-    @Override
-    public long getItemId(int i) {
-        return i;
-    }
-
-    @Override
-    public View getView(int i, View view, ViewGroup viewGroup) {
-        if(inflater == null)
-            inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        if(view == null){
-            view = inflater.inflate(R.layout.item_tourist_spot,null);
-        }
-
-        ImageView imageView = view.findViewById(R.id.list_image);
-        TextView nameview = view.findViewById(R.id.list_name);
-        TextView locview = view.findViewById(R.id.list_location);
-        TextView catview = view.findViewById(R.id.list_category);
-
-        Glide.with(view)
-                .load(restaurants.get(i).getFirstImage())
+        Glide.with(holder.itemView.getContext())
+                .load(restaurants.get(position).getFirstImage())
                 .placeholder(R.drawable.default_profile_image)
                 .error(R.drawable.default_profile_image)
                 .fallback(R.drawable.default_profile_image)
-                .into(imageView);
+                .into(holder.imgView);
+        holder.nameview.setText(filtered.get(position).getTitle());
+        holder.locview.setText(filtered.get(position).getAddr1());
+        holder.catview.setText(filtered.get(position).getAddr2());
+    }
 
-        nameview.setText(restaurants.get(i).getTitle());
-        locview.setText(restaurants.get(i).getAddr1());
-        catview.setText(restaurants.get(i).getAddr2());
+    @Override
+    public int getItemCount() {
+        return filtered.size();
+    }
 
-        return view;
+    @Override
+    public Filter getFilter() {
+        return filter;
+    }
+
+    private class RestaurantFilter extends Filter {
+
+        @Override
+        protected FilterResults performFiltering(CharSequence charSequence) {
+            FilterResults results = new FilterResults();
+            ArrayList<Restaurant> filteredList = new ArrayList<>();
+
+            if (charSequence == null || charSequence.length() == 0) {
+                filteredList.addAll(restaurants);
+            } else {
+                String filterString = charSequence.toString().toLowerCase();
+                String filterableString;
+                for (int i = 0; i < restaurants.size(); i++) {
+                    filterableString = restaurants.get(i).getTitle();
+                    if (filterableString.contains(filterString)) {
+                        filteredList.add(restaurants.get(i));
+                    }
+                }
+
+            }
+            results.values = filteredList;
+
+            return results;
+        }
+
+        @Override
+        protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+            filtered.clear();
+            filtered.addAll((ArrayList<Restaurant>) filterResults.values);
+            notifyDataSetChanged();
+        }
+    }
+
+    public interface OnItemClickListener {
+        void onItemClick(View v, int pos);
+    }
+
+    private OnItemClickListener mListener = null;
+
+    public void setOnItemClickListener(OnItemClickListener listener) {
+        this.mListener = listener;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView nameview;
+        public TextView locview;
+        public TextView catview;
+        public ImageView imgView;
+
+        public ViewHolder(Context context, @NonNull View itemView) {
+            super(itemView);
+
+            nameview = itemView.findViewById(R.id.list_name);
+            locview = itemView.findViewById(R.id.list_location);
+            catview = itemView.findViewById(R.id.list_category);
+
+            ImageView bookmarkbtn = itemView.findViewById(R.id.bookmarkBtn);
+            imgView = itemView.findViewById(R.id.list_image);
+
+
+            bookmarkbtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    bookmarkbtn.setActivated(!bookmarkbtn.isActivated());
+                    int pos = getAdapterPosition();
+                    writeBookmark(pos);
+                    Log.d("테스트",filtered.get(pos).getTitle());
+                }
+            });
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    int pos = getAdapterPosition();
+                    if (pos != RecyclerView.NO_POSITION) {
+                        if (mListener != null) {
+                            mListener.onItemClick(view, pos);
+                        }
+                    }
+                }
+            });
+
+
+        }
+
+
+    }
+
+    private void writeBookmark(int position){
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        mDatabase = FirebaseFirestore.getInstance();
+        Map<String, Object> info = new HashMap<>();
+        info.put("name", filtered.get(position).getTitle());
+        info.put("type", "식당");
+        info.put("address", filtered.get(position).getAddr1());
+        info.put("position_x", filtered.get(position).getMapX());
+        info.put("position_y", filtered.get(position).getMapY());
+        info.put("serialNumber", filtered.get(position).getContentID());
+        info.put("image", filtered.get(position).getFirstImage());
+        info.put("tel", filtered.get(position).getNumber());
+
+        String type = "식당";
+
+        String position_x = filtered.get(position).getMapX();
+        String position_y = filtered.get(position).getMapY();
+        String serialNumber = filtered.get(position).getContentID();
+        String image = filtered.get(position).getFirstImage();
+        String tel = filtered.get(position).getNumber();
+//        Restaurant br_info = new Restaurant(serialNumber, address, address, image, image, position_x, position_y, name, tel);
+        String userId = user.getUid();
+        mDatabase.collection("BookmarkItem").document(userId).collection(filtered.get(position).getContentID()).add(info);
+
+
+
     }
 }
