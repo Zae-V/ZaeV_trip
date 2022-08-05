cf73555cf3e2a8f114af545996960cce363c3758
dajeongPark-dev <jul897813@gmail.com>
2022-08-05 21:42:44 +0900

    Update lodging filter

--- app/src/main/java/com/example/ZaeV_trip/Lodging/LodgingAdapter.java
+++ app/src/main/java/com/example/ZaeV_trip/Lodging/LodgingAdapter.java
@@ -1,10 +1,13 @@
 package com.example.ZaeV_trip.Lodging;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -15,11 +18,14 @@ import com.example.ZaeV_trip.model.Lodging;
 
 import java.util.ArrayList;
 
-public class LodgingAdapter extends BaseAdapter {
+public class LodgingAdapter extends BaseAdapter implements Filterable {
     Context context;
     LayoutInflater inflater;
 
     ArrayList<Lodging> lodgings = new ArrayList<>();
+    ArrayList<Lodging> filteredlodgingsList = lodgings ;
+
+    Filter listFilter;
 
     public LodgingAdapter() {
 
@@ -34,18 +40,21 @@ public class LodgingAdapter extends BaseAdapter {
         this.lodgings.add(lodgings);
     }
 
-    public void addItem(Lodging item){
+    // 아이템 데이터 추가를 위한 함수. 개발자가 원하는대로 작성 가능.
+    public void addItem(Lodging item) {
         lodgings.add(item);
     }
 
+    // Adapter에 사용되는 데이터의 개수를 리턴. : 필수 구현
     @Override
     public int getCount() {
-        return lodgings.size();
+        return filteredlodgingsList.size() ;
     }
 
+    // 지정한 위치(position)에 있는 데이터 리턴 : 필수 구현
     @Override
-    public Object getItem(int i) {
-        return lodgings.get(i);
+    public Object getItem(int position) {
+        return filteredlodgingsList.get(position) ;
     }
 
     @Override
@@ -54,30 +63,85 @@ public class LodgingAdapter extends BaseAdapter {
     }
 
     @Override
-    public View getView(int i, View view, ViewGroup viewGroup) {
+    public View getView(int position, View view, ViewGroup viewGroup) {
         if(inflater == null)
             inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         if(view == null){
             view = inflater.inflate(R.layout.item_lodging,null);
         }
 
+        // 화면에 표시될 View(Layout이 inflate된)으로부터 위젯에 대한 참조 획득
         ImageView imageView = view.findViewById(R.id.list_image);
         TextView nameview = view.findViewById(R.id.list_name);
         TextView locview = view.findViewById(R.id.list_location);
-        TextView catview = view.findViewById(R.id.list_date);
+        //TextView catview = view.findViewById(R.id.list_date);
 
         Glide.with(view)
-                .load(lodgings.get(i).getFirstImage())
+                .load(lodgings.get(position).getFirstImage())
                 .placeholder(R.drawable.default_profile_image)
                 .error(R.drawable.default_profile_image)
                 .fallback(R.drawable.default_profile_image)
                 .into(imageView);
 
-        nameview.setText(lodgings.get(i).getTitle());
-        locview.setText(lodgings.get(i).getAddr1());
-        catview.setText(lodgings.get(i).getStartDate()+"~"+lodgings.get(i).getEndDate());
+        // Data Set(filteredlodgingsList)에서 position에 위치한 데이터 참조 획득
+        Lodging lodging = filteredlodgingsList.get(position);
+
+        nameview.setText(lodgings.get(position).getTitle());
+        locview.setText(lodgings.get(position).getAddr1());
+        //catview.setText(lodgings.get(position).getStartDate()+"~"+lodgings.get(position).getEndDate());
 
         return view;
+    }
+
+    
+    @Override
+    public Filter getFilter() {
+        if (listFilter == null) {
+            listFilter = new ListFilter() ;
+        }
+
+        return listFilter ;
+    }
+
+    private class ListFilter extends Filter {
+
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint) {
+            FilterResults results = new FilterResults() ;
+
+            if (constraint == null || constraint.length() == 0) {
+                results.values = lodgings ;
+                results.count = lodgings.size() ;
+            } else {
+                ArrayList<Lodging> itemList = new ArrayList<Lodging>() ;
+
+                for (Lodging item : lodgings) {
+                    if (item.getTitle().toUpperCase().contains(constraint.toString().toUpperCase()) ||
+                            item.getAddr1().toUpperCase().contains(constraint.toString().toUpperCase()))
+                    {
+                        itemList.add(item) ;
+                    }
+                }
+
+                results.values = itemList ;
+                results.count = itemList.size() ;
+            }
+            return results;
+        }
+
+        @Override
+        protected void publishResults(CharSequence constraint, FilterResults results) {
+
+            // update listview by filtered data list.
+            filteredlodgingsList = (ArrayList<Lodging>) results.values ;
+
+            // notify
+            if (results.count > 0) {
+                notifyDataSetChanged() ;
+            } else {
+                notifyDataSetInvalidated() ;
+            }
+        }
     }
 }
 
