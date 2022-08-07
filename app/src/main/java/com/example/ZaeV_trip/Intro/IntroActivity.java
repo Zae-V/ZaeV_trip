ff616f1c532ea0f0343aa538283e8b261c71674f
CHOSIYEON <tldusdk5@naver.com>
2022-08-07 21:05:00 +0900

    refactor: Intro Activity 코드 정리

--- app/src/main/java/com/example/ZaeV_trip/Intro/IntroActivity.java
+++ app/src/main/java/com/example/ZaeV_trip/Intro/IntroActivity.java
@@ -103,62 +103,7 @@ public class IntroActivity extends AppCompatActivity {
             }
         });
         introText.startAnimation(fadeIn);
-
-        Function2<OAuthToken, Throwable, Unit> callback = new Function2<OAuthToken, Throwable, Unit>() {
-            @Override
-            public Unit invoke(OAuthToken oAuthToken, Throwable throwable) {
-                if (oAuthToken != null) {
-                    UserApiClient.getInstance().me(new Function2<User, Throwable, Unit>() {
-                        @Override
-                        public Unit invoke(User user, Throwable throwable) {
-                            if (user != null) {
-                                Log.d(TAG, "invoke: id=" + user.getId());
-                                Log.d(TAG, "invoke: email=" + user.getKakaoAccount().getEmail());
-
-                                String userName = user.getKakaoAccount().getProfile().getNickname();
-                                String userEmail = user.getKakaoAccount().getEmail();
-                                String userPassword = user.getId() + user.getKakaoAccount().getEmail();
-                                ArrayList bookmarkList = new ArrayList();
-                                ArrayList currentPosition = new ArrayList();
-                                String profileImage = user.getKakaoAccount().getProfile().getProfileImageUrl();
-                                Boolean notification = false;
-
-                                Users newUser = new Users(userName, userEmail, bookmarkList, currentPosition, profileImage, notification);
-
-                                // 이메일 중복 체크
-                                mFirestore.collection("User").document(userEmail)
-                                        .get().
-                                        addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-                                            @Override
-                                            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                                                if (task.isSuccessful()) {
-                                                    DocumentSnapshot document = task.getResult();
-                                                    if (document.exists()) {
-                                                        checkedEmailDuplicate(true, newUser, userPassword);
-                                                    } else {
-                                                        checkedEmailDuplicate(false, newUser, userPassword);
-                                                    }
-                                                } else {
-                                                    Log.d(TAG, "get failed with ", task.getException());
-                                                }
-                                            }
-                                        });
-                            }
-                            if (throwable != null) {
-                                Log.d(TAG, "invoke: " + throwable.getLocalizedMessage());
-                            }
-                            return null;
-                        }
-                    });
-                }
-                if (throwable != null) {
-                    // TBD
-                    Log.d(TAG, "invoke: " + throwable.getLocalizedMessage());
-                }
-                return null;
-            }
-        };
-
+        
         kakaoJoinBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -173,54 +118,5 @@ public class IntroActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-    }
-
-    public void signIn(Users user, String password) {
-        mAuth.signInWithEmailAndPassword(user.userEmail, password).addOnCompleteListener(IntroActivity.this, new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if(task.isSuccessful()){
-                    Log.d(TAG, "로그인 성공");
-                    MySharedPreferences.saveUserInfo(IntroActivity.this, user);
-                    Intent intent = new Intent(IntroActivity.this, MainActivity.class);
-                    startActivity(intent);
-                }else{
-                    Log.d(TAG, "로그인 실패");
-                    Log.d(TAG, String.valueOf(task.getException()));
-
-                    Toast.makeText(getApplicationContext(), "이미 존재하는 이메일이 있습니다.", Toast.LENGTH_SHORT).show();
-                    // 이미 아이디 존재할 때 따로 처리 //
-                }
-            }
-        });
-    }
-
-    public void signUp(Users user, String password) {
-        mAuth.createUserWithEmailAndPassword(user.userEmail, password).addOnCompleteListener(IntroActivity.this, new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if (task.isSuccessful()) {
-                    Log.d(TAG, "회원가입 성공");
-                    mFirestore.collection("User").document(user.userEmail).set(user);
-                    MySharedPreferences.saveUserInfo(IntroActivity.this, user);
-
-                    Intent intent = new Intent(IntroActivity.this, MainActivity.class);
-                    startActivity(intent);
-                } else {
-                    Log.d(TAG, "회원가입 실패");
-                    Log.d(TAG, String.valueOf(task.getException()));
-                }
-            }
-        });
-    }
-
-    public void checkedEmailDuplicate(Boolean emailDuplicated,Users user, String password) {
-        if (emailDuplicated) {
-            Log.d(TAG, "중복된 이메일이 있습니다");
-            signIn(user, password);
-        } else {
-            Log.d(TAG, "중복된 이메일이 없습니다.");
-            signUp(user, password);
-        }
     }
 }
