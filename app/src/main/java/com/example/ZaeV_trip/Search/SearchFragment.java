2384d7416d48bd363d99eb8380e7beb949785b9b
Seyiul <63714074+Seyiul@users.noreply.github.com>
2022-08-09 21:16:23 +0900

    Update SearchFragment.java

--- app/src/main/java/com/example/ZaeV_trip/Search/SearchFragment.java
+++ app/src/main/java/com/example/ZaeV_trip/Search/SearchFragment.java
@@ -25,6 +25,13 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class SearchFragment extends Fragment {
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        BottomNavigationView navView = getActivity().findViewById(R.id.bottom_navigation);
+        navView.setVisibility(View.VISIBLE);
+    }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -34,7 +41,6 @@ public class SearchFragment extends Fragment {
 
         BottomNavigationView navView = getActivity().findViewById(R.id.bottom_navigation);
         navView.setVisibility(View.GONE);
-
 
         ArrayList<String> CityName = new ArrayList<String>(Stream.of("전체", "강남구", "강동구", "강북구", "강서구", "관악구", "광진구", "구로구", "금천구", "노원구", "도봉구", "동대문구",
                 "동작구", "마포구", "서대문구", "서초구", "성동구", "성북구", "송파구", "양천구", "영등포구", "용산구", "은평구", "종로구", "종구", "중랑구").collect(Collectors.toList()));
