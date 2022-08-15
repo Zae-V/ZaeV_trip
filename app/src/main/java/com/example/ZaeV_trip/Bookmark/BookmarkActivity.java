b17a2540fba9e6071c77f4bca9ba3f305f63f968
Seyiul <63714074+Seyiul@users.noreply.github.com>
2022-08-15 18:44:02 +0900

    Bookmark Activity 구현

--- app/src/main/java/com/example/ZaeV_trip/Bookmark/BookmarkActivity.java
+++ app/src/main/java/com/example/ZaeV_trip/Bookmark/BookmarkActivity.java
@@ -16,15 +16,25 @@ import android.util.Log;
 import com.example.ZaeV_trip.Cafe.CafeActivity;
 import com.example.ZaeV_trip.Cafe.CafeAdapter;
 import com.example.ZaeV_trip.Festival.FestivalAdapter;
+import com.example.ZaeV_trip.Lodging.LodgingAdapter;
+import com.example.ZaeV_trip.Plogging.PloggingAdapter;
 import com.example.ZaeV_trip.Profile.ProfileActivity;
 import com.example.ZaeV_trip.Main.MainActivity;
 import com.example.ZaeV_trip.R;
 import com.example.ZaeV_trip.Restaurant.RestaurantAdapter;
+import com.example.ZaeV_trip.Reusable.ReusableAdapter;
 import com.example.ZaeV_trip.Schedule.TravelActivity;
 import com.example.ZaeV_trip.Search.SearchActivity;
+import com.example.ZaeV_trip.TouristSpot.TouristSpotAdapter;
+import com.example.ZaeV_trip.ZeroWaste.ZeroWasteAdapter;
 import com.example.ZaeV_trip.model.Cafe;
 import com.example.ZaeV_trip.model.Festival;
+import com.example.ZaeV_trip.model.Lodging;
+import com.example.ZaeV_trip.model.Plogging;
 import com.example.ZaeV_trip.model.Restaurant;
+import com.example.ZaeV_trip.model.Reusable;
+import com.example.ZaeV_trip.model.TouristSpot;
+import com.example.ZaeV_trip.model.ZeroWaste;
 import com.example.ZaeV_trip.util.ItemTouchHelperCallback;
 import com.example.ZaeV_trip.util.getXmlData;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -51,6 +61,11 @@ public class BookmarkActivity extends AppCompatActivity {
     CafeAdapter listAdapter;
     FestivalAdapter listAdapter1;
     RestaurantAdapter listAdapter2;
+    TouristSpotAdapter listAdapter3;
+    LodgingAdapter listAdapter4;
+    ReusableAdapter listAdapter5;
+    ZeroWasteAdapter listAdapter6;
+    PloggingAdapter listAdapter7;
 
     ItemTouchHelper helper;
 
@@ -58,6 +73,12 @@ public class BookmarkActivity extends AppCompatActivity {
     ArrayList<Cafe> bookmarkedItems = new ArrayList<Cafe>();
     ArrayList<Festival> bookmarkedItems1 = new ArrayList<Festival>();
     ArrayList<Restaurant> bookmarkedItems2 = new ArrayList<Restaurant>();
+    ArrayList<TouristSpot> bookmarkedItems3 = new ArrayList<>();
+    ArrayList<Lodging> bookmarkedItems4 = new ArrayList<>();
+    ArrayList<Reusable> bookmarkedItems5 = new ArrayList<>();
+    ArrayList<ZeroWaste> bookmarkedItems6 = new ArrayList<>();
+    ArrayList<Plogging> bookmarkedItems7 = new ArrayList<>();
+
 
     FirebaseFirestore mDatabase = FirebaseFirestore.getInstance();
     String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
@@ -154,6 +175,120 @@ public class BookmarkActivity extends AppCompatActivity {
                 }
             }
         });
+
+        CollectionReference collection3 = mDatabase.collection("BookmarkItem").document(uid).collection("touristSpot");
+        collection3.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if(task.isSuccessful()){
+                    QuerySnapshot query = task.getResult();
+                    for(QueryDocumentSnapshot document : query){
+                        TouristSpot touristSpot = new TouristSpot("","","","","","","","");
+                        touristSpot.setContentID(String.valueOf(document.getData().get("serialNumber")));
+                        touristSpot.setAddr1(String.valueOf(document.getData().get("address")));
+                        touristSpot.setTitle(String.valueOf(document.getData().get("name")));
+                        touristSpot.setFirstImage(String.valueOf(document.getData().get("image")));
+                        touristSpot.setMapX(String.valueOf(document.getData().get("position_x")));
+                        touristSpot.setMapY(String.valueOf(document.getData().get("position_y")));
+                        bookmarkedItems3.add(touristSpot);
+                    }
+
+                    listAdapter3 = new TouristSpotAdapter(BookmarkActivity.this, bookmarkedItems3);
+                    mConcatAdapter.addAdapter(listAdapter3);
+                    bookmarkRecyclerView.setAdapter(mConcatAdapter);
+                }
+            }
+        });
+
+        CollectionReference collection4 = mDatabase.collection("BookmarkItem").document(uid).collection("lodging");
+        collection4.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if(task.isSuccessful()){
+                    QuerySnapshot query = task.getResult();
+                    for(QueryDocumentSnapshot document : query){
+                        Lodging lodging = new Lodging("","","","","","","","");
+                        lodging.setContentID(String.valueOf(document.getData().get("serialNumber")));
+                        lodging.setAddr1(String.valueOf(document.getData().get("address")));
+                        lodging.setTitle(String.valueOf(document.getData().get("name")));
+                        lodging.setFirstImage(String.valueOf(document.getData().get("image")));
+                        lodging.setMapX(String.valueOf(document.getData().get("position_x")));
+                        lodging.setMapY(String.valueOf(document.getData().get("position_y")));
+                        bookmarkedItems4.add(lodging);
+                    }
+
+                    listAdapter4 = new LodgingAdapter(BookmarkActivity.this, bookmarkedItems4);
+                    mConcatAdapter.addAdapter(listAdapter4);
+                    bookmarkRecyclerView.setAdapter(mConcatAdapter);
+                }
+            }
+        });
+
+        CollectionReference collection5 = mDatabase.collection("BookmarkItem").document(uid).collection("reusable");
+        collection5.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if(task.isSuccessful()){
+                    QuerySnapshot query = task.getResult();
+                    for(QueryDocumentSnapshot document : query){
+                        Reusable reusable = new Reusable("","","","","");
+                        reusable.setName(String.valueOf(document.getData().get("name")));
+                        reusable.setMapX(String.valueOf(document.getData().get("position_x")));
+                        reusable.setMapY(String.valueOf(document.getData().get("position_y")));
+                        reusable.setLocation(String.valueOf(document.getData().get("address")));
+                        reusable.setReason(String.valueOf(document.getData().get("reason")));
+                        bookmarkedItems5.add(reusable);
+                    }
+
+                    listAdapter5 = new ReusableAdapter(BookmarkActivity.this, bookmarkedItems5);
+                    mConcatAdapter.addAdapter(listAdapter5);
+                    bookmarkRecyclerView.setAdapter(mConcatAdapter);
+                }
+            }
+        });
+
+        CollectionReference collection6 = mDatabase.collection("BookmarkItem").document(uid).collection("zeroWaste");
+        collection6.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if(task.isSuccessful()){
+                    QuerySnapshot query = task.getResult();
+                    for(QueryDocumentSnapshot document : query){
+                        ZeroWaste zeroWaste = new ZeroWaste("","","","","");
+                        zeroWaste.setName(String.valueOf(document.getData().get("name")));
+                        zeroWaste.setMapX(String.valueOf(document.getData().get("position_x")));
+                        zeroWaste.setMapY(String.valueOf(document.getData().get("position_y")));
+                        zeroWaste.setLocation(String.valueOf(document.getData().get("address")));
+                        zeroWaste.setReason(String.valueOf(document.getData().get("reason")));
+                        bookmarkedItems6.add(zeroWaste);
+                    }
+
+                    listAdapter6 = new ZeroWasteAdapter(BookmarkActivity.this, bookmarkedItems6);
+                    mConcatAdapter.addAdapter(listAdapter6);
+                    bookmarkRecyclerView.setAdapter(mConcatAdapter);
+                }
+            }
+        });
+
+        CollectionReference collection7 = mDatabase.collection("BookmarkItem").document(uid).collection("plogging");
+        collection7.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if(task.isSuccessful()){
+                    QuerySnapshot query = task.getResult();
+                    for(QueryDocumentSnapshot document : query){
+                        Plogging plogging = new Plogging();
+                        plogging.setCrsKorNm(String.valueOf(document.getData().get("name")));
+                        bookmarkedItems7.add(plogging);
+                    }
+
+                    listAdapter7 = new PloggingAdapter(BookmarkActivity.this, bookmarkedItems7);
+                    mConcatAdapter.addAdapter(listAdapter7);
+                    bookmarkRecyclerView.setAdapter(mConcatAdapter);
+                }
+            }
+        });
+
 
 //        //ItemTouchHelper 생성
 //        ItemTouchHelper.Callback callback = new ItemTouchHelperCallback(listAdapter);
