951da11374e7d22fce747daa7adfa9e065768e08
Seyiul <63714074+Seyiul@users.noreply.github.com>
2022-08-19 20:49:23 +0900

    Update SetScheduleFragment.java
    - image 가져오기

--- app/src/main/java/com/example/ZaeV_trip/Schedule/SetScheduleFragment.java
+++ app/src/main/java/com/example/ZaeV_trip/Schedule/SetScheduleFragment.java
@@ -1,7 +1,10 @@
 package com.example.ZaeV_trip.Schedule;
 
+import static android.app.Activity.RESULT_OK;
+
 import android.app.Dialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -13,6 +16,10 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -20,6 +27,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.example.ZaeV_trip.Main.MainActivity;
 import com.example.ZaeV_trip.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
@@ -31,8 +39,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class SetScheduleFragment extends Fragment {
+    private static int RESULT_LOAD_IMAGE = 1;
 
     TextView title;
+    ImageView editTitleImageView;
+    ImageView travleImageView;
+
     Integer Dday;
     Date startDate;
     Date endDate;
@@ -41,7 +53,16 @@ public class SetScheduleFragment extends Fragment {
     String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
 
     String docId;
+    int SELECT_PICTURE = 200;
 
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        TravelActivity travelActivity = (TravelActivity) getActivity();
+        travelActivity.fab.setVisibility(View.GONE);
+        travelActivity.bottomNavigationView.setVisibility(View.GONE);
+    }
 
     @Nullable
     @Override
@@ -73,7 +94,7 @@ public class SetScheduleFragment extends Fragment {
         }
 
 
-        ImageView editTitleImageView = v.findViewById(R.id.editTitleImageView);
+        editTitleImageView = v.findViewById(R.id.editTitleImageView);
         title = v.findViewById(R.id.travel_title);
         editTitleImageView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -82,11 +103,14 @@ public class SetScheduleFragment extends Fragment {
             }
         });
 
-        ImageView travleImageView = v.findViewById(R.id.travelImageView);
+        travleImageView = v.findViewById(R.id.travelImageView);
         travleImageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                launcher.launch(intent);
             }
         });
 
@@ -123,7 +147,7 @@ public class SetScheduleFragment extends Fragment {
             public void onClick(View v){
                 title.setText(setTitle.getText());
                 dialog.hide();
-                updateScheduleDB(String.valueOf(setTitle.getText()));
+                updateName(String.valueOf(setTitle.getText()));
 
             }
         });
@@ -150,11 +174,36 @@ public class SetScheduleFragment extends Fragment {
 
     }
 
-    public void updateScheduleDB(String name){
+    public void updateName(String name){
         if(docId != null){
             db.collection("Schedule").document(uid).collection("schedule").document(docId).update("name",name);
         }
 
     }
+
+    public void updateImg(String uri){
+        if(docId != null){
+            db.collection("Schedule").document(uid).collection("schedule").document(docId).update("img",uri);
+        }
+
+    }
+
+    ActivityResultLauncher<Intent> launcher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>()
+            {
+                @Override
+                public void onActivityResult(ActivityResult result)
+                {
+                    if (result.getResultCode() == RESULT_OK)
+                    {
+                        Intent intent = result.getData();
+                        Uri uri = intent.getData();
+//                        updateImg(uri.toString());
+                        Glide.with(getActivity())
+                                .load(uri)
+                                .into(travleImageView);
+                    }
+                }
+            });
 
 }
