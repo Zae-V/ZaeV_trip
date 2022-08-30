2a4f641a0b6a3c713477bbcdc8a11c7e46bcaea8
CHOSIYEON <tldusdk5@naver.com>
2022-08-31 08:46:11 +0900

    feat : 일정 기능 막기

--- app/src/main/java/com/zaev/ZaeV_trip/Schedule/TravelActivity.java
+++ app/src/main/java/com/zaev/ZaeV_trip/Schedule/TravelActivity.java
@@ -56,98 +56,97 @@ public class TravelActivity extends AppCompatActivity {
         bottomNavigationView.setSelectedItemId(R.id.travel);
 
 
-        // fab 버튼
-        fab = (FloatingActionButton)findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                // fab 버튼 누르면 calendar fragment 이동
-                FragmentManager fragmentManager = getSupportFragmentManager();
-                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-                CalendarFragment calendarFragment = new CalendarFragment();
-                fragmentTransaction.add(R.id.container_travel, calendarFragment);
-                fragmentTransaction.addToBackStack(null);
-                fragmentTransaction.commit();
-
-            }
-        });
-
-        bottomNavigationView.setVisibility(View.VISIBLE);
-        fab.setVisibility(View.VISIBLE);
-
-        //recyclerview
-        scheduleRecyclerView = (RecyclerView) findViewById(R.id.travelRecycler);
-        scheduleRecyclerView.setHasFixedSize(true);
-        listAdapter = new TravelListAdapter(items);
-
-
-
-        //ItemTouchHelper 생성
-        ItemTouchHelper.Callback callback = new ItemTouchHelperCallback(listAdapter);
-        ItemTouchHelper touchHelper = new ItemTouchHelper(callback);
-        touchHelper.attachToRecyclerView(scheduleRecyclerView);
-
-        //Adapter에 데이터 추가
-        db.collection("Schedule").document(uid).collection("schedule").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                if(task.isSuccessful()){
-                    QuerySnapshot query = task.getResult();
-                    for(QueryDocumentSnapshot document : query){
-                        TravelItem item = new TravelItem("","","");
-                        item.setName(String.valueOf(document.getData().get("name")));
-
-                        Timestamp startStamp = (Timestamp) document.getData().get("startDate");
-                        Date startDate = startStamp.toDate();
-                        Calendar calendar1 = new GregorianCalendar();
-                        calendar1.setTime(startDate);
-
-                        Timestamp endStamp = (Timestamp) document.getData().get("endDate");
-                        Date endDate = endStamp.toDate();
-                        Calendar calendar2 = new GregorianCalendar();
-                        calendar2.setTime(endDate);
-
-                        item.setDate(calendar1.get(Calendar.YEAR) + "." + String.valueOf(calendar1.get(Calendar.MONTH)+1) + "." +calendar1.get(Calendar.DAY_OF_MONTH)
-                        + " ~ " + calendar2.get(Calendar.YEAR) + "." + String.valueOf(calendar2.get(Calendar.MONTH)+1) + "." +calendar2.get(Calendar.DAY_OF_MONTH)
-                        );
-
-                        item.setImage(String.valueOf(document.getData().get("img")));
-
-                        listAdapter.addItem(item);
-                    }
-                    RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(TravelActivity.this);
-                    scheduleRecyclerView.setLayoutManager(mLayoutManager);
-                    scheduleRecyclerView.setItemAnimator(new DefaultItemAnimator());
-                    scheduleRecyclerView.setAdapter(listAdapter);
-                }
-            }
-        });
-
-        listAdapter.setOnItemClickListener(new OnTravelItemClickListener() {
-            @Override
-            public void onItemClick(TravelListAdapter.ItemViewHolder holder, View view, int position) {
-                TravelItem item = listAdapter.getItem(position);
-//                Toast.makeText(getApplicationContext(),"아이템 선택 " + item.getName(),
-//                        Toast.LENGTH_SHORT).show();
-                // 액티비티-> schedule fragment 전환
-                FragmentManager fragmentManager = getSupportFragmentManager();
-                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-                ScheduleFragment scheduleFragment = new ScheduleFragment();
-
-                Bundle bundle = new Bundle();
-                bundle.putInt("position",position);
-                bundle.putString("name", item.getName());
-                bundle.putString("date", item.getDate());
-                bundle.putString("img", item.getImage());
-                scheduleFragment.setArguments(bundle);
-
-                fragmentTransaction.replace(R.id.container_travel, scheduleFragment);
-                fragmentTransaction.addToBackStack(null);
-                fragmentTransaction.commit();
-                bottomNavigationView.setVisibility(View.GONE);
-            }
-
-        });
+//        // fab 버튼
+//        fab = (FloatingActionButton)findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View v){
+//                // fab 버튼 누르면 calendar fragment 이동
+//                FragmentManager fragmentManager = getSupportFragmentManager();
+//                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+//                CalendarFragment calendarFragment = new CalendarFragment();
+//                fragmentTransaction.add(R.id.container_travel, calendarFragment);
+//                fragmentTransaction.addToBackStack(null);
+//                fragmentTransaction.commit();
+//
+//            }
+//        });
+//
+//        bottomNavigationView.setVisibility(View.VISIBLE);
+//        fab.setVisibility(View.GONE); // 일정 막기
+//
+//        //recyclerview
+//        scheduleRecyclerView = (RecyclerView) findViewById(R.id.travelRecycler);
+//
+//        scheduleRecyclerView.setVisibility(View.GONE); // 비활성화
+//        scheduleRecyclerView.setHasFixedSize(true);
+//        listAdapter = new TravelListAdapter(items);
+//
+//
+//
+//        //ItemTouchHelper 생성
+//        ItemTouchHelper.Callback callback = new ItemTouchHelperCallback(listAdapter);
+//        ItemTouchHelper touchHelper = new ItemTouchHelper(callback);
+//        touchHelper.attachToRecyclerView(scheduleRecyclerView);
+//
+//        //Adapter에 데이터 추가
+//        db.collection("Schedule").document(uid).collection("schedule").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+//            @Override
+//            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+//                if(task.isSuccessful()){
+//                    QuerySnapshot query = task.getResult();
+//                    for(QueryDocumentSnapshot document : query){
+//                        TravelItem item = new TravelItem("","","");
+//                        item.setName(String.valueOf(document.getData().get("name")));
+//
+//                        Timestamp startStamp = (Timestamp) document.getData().get("startDate");
+//                        Date startDate = startStamp.toDate();
+//                        Calendar calendar1 = new GregorianCalendar();
+//                        calendar1.setTime(startDate);
+//
+//                        Timestamp endStamp = (Timestamp) document.getData().get("endDate");
+//                        Date endDate = endStamp.toDate();
+//                        Calendar calendar2 = new GregorianCalendar();
+//                        calendar2.setTime(endDate);
+//
+//                        item.setDate(calendar1.get(Calendar.YEAR) + "." + String.valueOf(calendar1.get(Calendar.MONTH)+1) + "." +calendar1.get(Calendar.DAY_OF_MONTH)
+//                        + " ~ " + calendar2.get(Calendar.YEAR) + "." + String.valueOf(calendar2.get(Calendar.MONTH)+1) + "." +calendar2.get(Calendar.DAY_OF_MONTH)
+//                        );
+//
+//                        item.setImage(String.valueOf(document.getData().get("img")));
+//
+//                        listAdapter.addItem(item);
+//                    }
+//                    RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(TravelActivity.this);
+//                    scheduleRecyclerView.setLayoutManager(mLayoutManager);
+//                    scheduleRecyclerView.setItemAnimator(new DefaultItemAnimator());
+//                    scheduleRecyclerView.setAdapter(listAdapter);
+//                }
+//            }
+//        });
+//
+//        listAdapter.setOnItemClickListener(new OnTravelItemClickListener() {
+//            @Override
+//            public void onItemClick(TravelListAdapter.ItemViewHolder holder, View view, int position) {
+//                TravelItem item = listAdapter.getItem(position);
+//                FragmentManager fragmentManager = getSupportFragmentManager();
+//                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+//                ScheduleFragment scheduleFragment = new ScheduleFragment();
+//
+//                Bundle bundle = new Bundle();
+//                bundle.putInt("position",position);
+//                bundle.putString("name", item.getName());
+//                bundle.putString("date", item.getDate());
+//                bundle.putString("img", item.getImage());
+//                scheduleFragment.setArguments(bundle);
+//
+//                fragmentTransaction.replace(R.id.container_travel, scheduleFragment);
+//                fragmentTransaction.addToBackStack(null);
+//                fragmentTransaction.commit();
+//                bottomNavigationView.setVisibility(View.GONE);
+//            }
+//
+//        });
 
         //Perform ItemSelectedListener
         bottomNavigationView.setOnItemSelectedListener(item -> {
@@ -198,7 +197,7 @@ public class TravelActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         bottomNavigationView.setVisibility(View.VISIBLE);
-        fab.setVisibility(View.VISIBLE);
+//        fab.setVisibility(View.VISIBLE);
     }
 
     // 연결된 fragment 끼리 전환
