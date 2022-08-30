23b89be430c7ec446eb61e9e9835a3f1c51cf587
jul897813@gmail.com <jul897813@gmail.com>
2022-08-31 01:22:02 +0900

    Update ZeroWaste

--- app/src/main/java/com/zaev/ZaeV_trip/model/ZeroWaste.java
+++ app/src/main/java/com/zaev/ZaeV_trip/model/ZeroWaste.java
@@ -151,10 +151,18 @@ public class ZeroWaste implements Serializable {
     }
 
 
-    public ZeroWaste(String name, String Addr1, String contentID, String themeSubID) {
+    public ZeroWaste(String name, String Addr1, String contentID, String themeSubID, String mapX, String mapY, String telephone, String image, String time, String homepage, String activity, String menu) {
         this.name = name;
         this.Addr1 = Addr1;
         this.contentID = contentID;
         this.themeSubID = themeSubID;
+        this.mapX = mapX;
+        this.mapY = mapY;
+        this.telephone = telephone;
+        this.image = image;
+        this.time = time;
+        this.homepage = homepage;
+        this.activity = activity;
+        this.menu = menu;
     }
 }
